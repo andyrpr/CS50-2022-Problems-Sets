6ca45f6109549cd464493d08382c098ae55fa4bd
arpr <andyrenework@gmail.com>
2024-10-31 18:49:29 +0300

    Create figlet.py

---
+++ Problem-Set-4/figlet.py
@@ -0,0 +1,31 @@
+import random
+import sys
+
+from pyfiglet import Figlet
+
+figlet = Figlet()
+
+figlet.getFonts()
+
+if len(sys.argv) == 1:
+    is_random = True
+elif len(sys.argv) == 3 and (sys.argv[1]) == "-f" or sys.argv[1] == "--font":
+    is_random = False
+else:
+    print("Invalid Usage")
+    sys.exit(1)
+
+if is_random == False:
+    try:
+        figlet.setFont(font=sys.argv[2])
+    except:
+        print("Invalid Usage")
+        sys.exit(1)
+    else:
+        font = random.choice(figlet.getFonts())
+    
+text = input("Input")
+print("Output:", figlet.renderText(text))     
+    
+    
+
